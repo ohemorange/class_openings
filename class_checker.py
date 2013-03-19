0f438c8451b608dca572db3ef9d8db6f96bba04f
Erica Portnoy <ebportnoy@gmail.com>
2013-03-19 19:51:39 -0400

    back to the original version

--- class_checker.py
+++ class_checker.py
@@ -7,16 +7,16 @@ toaddrs = 'eportnoy@princeton.edu'
 
 msg = None
 
-# url = "http://registrar.princeton.edu/course-offerings/course_details.xml?courseid=002075&term=1134"
-# url_opener = urllib2.urlopen(url)
-# page = url_opener.read()
-# soup = BeautifulSoup(page)
+url = "http://registrar.princeton.edu/course-offerings/course_details.xml?courseid=002075&term=1134"
+url_opener = urllib2.urlopen(url)
+page = url_opener.read()
+soup = BeautifulSoup(page)
 
-# cols = soup.find("table").find_all("tr")[4].find_all("td")
-# enrolled = int(str(cols[5])[30:32])
-# limit = int(str(cols[5])[57:59])
-# if enrolled < limit:
-#     msg = "436 "
+cols = soup.find("table").find_all("tr")[4].find_all("td")
+enrolled = int(str(cols[5])[30:32])
+limit = int(str(cols[5])[57:59])
+if enrolled < limit:
+    msg = "436 "
 
 url = "http://registrar.princeton.edu/course-offerings/course_details.xml?courseid=012261&term=1134"
 url_opener = urllib2.urlopen(url)
@@ -31,19 +31,19 @@ if enrolled != 26:
     else:
         msg += "TRA "
 
-# url = "http://registrar.princeton.edu/course-offerings/course_details.xml?courseid=010457&term=1134"
-# url_opener = urllib2.urlopen(url)
-# page = url_opener.read()
-# soup = BeautifulSoup(page)
+url = "http://registrar.princeton.edu/course-offerings/course_details.xml?courseid=010457&term=1134"
+url_opener = urllib2.urlopen(url)
+page = url_opener.read()
+soup = BeautifulSoup(page)
 
-# cols = soup.find("table").find_all("tr")[1].find_all("td")
-# enrolled = int(str(cols[5])[30:33])
-# limit = int(str(cols[5])[58:61])
-# if enrolled < limit:
-#     if msg == None:
-#         msg = "SOC 204 "
-#     else:
-#         msg += "SOC 204 "
+cols = soup.find("table").find_all("tr")[1].find_all("td")
+enrolled = int(str(cols[5])[30:33])
+limit = int(str(cols[5])[58:61])
+if enrolled < limit:
+    if msg == None:
+        msg = "SOC 204 "
+    else:
+        msg += "SOC 204 "
 
 url = "http://registrar.princeton.edu/course-offerings/course_details.xml?courseid=010455&term=1134"
 url_opener = urllib2.urlopen(url)
